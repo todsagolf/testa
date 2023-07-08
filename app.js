1e006e0bfabd32eb61db3f320acf4058447dfd0f
todsagolf <todsagolf@gmail.com>
2023-07-09 00:03:03 +0700

    update2

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res) => {
 
